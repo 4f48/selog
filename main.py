f9bfbd678efad9ed9b53800e9a85a96037ee14e6
Oliver Pirger <oliver@pirger.eu>
2025-10-27 17:10:00 +0100

    read serial

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from serial import Serial, PARITY_NONE, STOPBITS_ONE, EIGHTBITS
+
+
 def main():
-    print("Hello from selog!")
+    port = input("serial port: ")
+    serial = Serial(
+        port=port,
+        baudrate=115200,
+        parity=PARITY_NONE,
+        stopbits=STOPBITS_ONE,
+        bytesize=EIGHTBITS,
+        timeout=5.0,
+    )
+
+    while True:
+        line = serial.readline()
+        if line:
+            print(line.decode("utf-8"))
 
 
 if __name__ == "__main__":
